23eefc56095543a59898feba4455bab5db00bb42
David Domínguez Mejías <davidomej@hotmail.com>
2022-10-12 11:54:02 +0100

    Last commit
    Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaPeliculasComponent } from './components/lista-peliculas/lista-peliculas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FootComponent } from './components/foot/foot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaPeliculasComponent
+    ListaPeliculasComponent,
+    HeaderComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/lista-peliculas/lista-peliculas.component.ts
+++ src/app/components/lista-peliculas/lista-peliculas.component.ts
@@ -18,6 +18,15 @@ export class ListaPeliculasComponent implements OnInit {
     this.peliculas=[
       {titulo:"Tiburon", director:"Spielberg", imagen:"https://mewmagazine.es/wp-content/uploads/2021/01/tiburon-pelicula.jpg"},
       {titulo:"Alien", director:"Ridley", imagen:"https://images.ecestaticos.com/YIuxgKfHfLRlUtOCuUie_i5b7C8=/0x0:1022x765/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fb7f%2F5ec%2F3f5%2Fb7f5ec3f51564bd3af46a5609457d6b9.jpg"},
+      {titulo:"El Exorcista", director:"William Friedkin", imagen:"https://www.ecartelera.com/carteles/5100/5147/001_m.jpg"},
+      {titulo:"Tiburon", director:"Spielberg", imagen:"https://mewmagazine.es/wp-content/uploads/2021/01/tiburon-pelicula.jpg"},
+      {titulo:"Alien", director:"Ridley", imagen:"https://images.ecestaticos.com/YIuxgKfHfLRlUtOCuUie_i5b7C8=/0x0:1022x765/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fb7f%2F5ec%2F3f5%2Fb7f5ec3f51564bd3af46a5609457d6b9.jpg"},
+      {titulo:"El Exorcista", director:"William Friedkin", imagen:"https://www.ecartelera.com/carteles/5100/5147/001_m.jpg"},
+      {titulo:"Tiburon", director:"Spielberg", imagen:"https://mewmagazine.es/wp-content/uploads/2021/01/tiburon-pelicula.jpg"},
+      {titulo:"Alien", director:"Ridley", imagen:"https://images.ecestaticos.com/YIuxgKfHfLRlUtOCuUie_i5b7C8=/0x0:1022x765/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fb7f%2F5ec%2F3f5%2Fb7f5ec3f51564bd3af46a5609457d6b9.jpg"},
+      {titulo:"El Exorcista", director:"William Friedkin", imagen:"https://www.ecartelera.com/carteles/5100/5147/001_m.jpg"},
+      {titulo:"Tiburon", director:"Spielberg", imagen:"https://mewmagazine.es/wp-content/uploads/2021/01/tiburon-pelicula.jpg"},
+      {titulo:"Alien", director:"Ridley", imagen:"https://images.ecestaticos.com/YIuxgKfHfLRlUtOCuUie_i5b7C8=/0x0:1022x765/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fb7f%2F5ec%2F3f5%2Fb7f5ec3f51564bd3af46a5609457d6b9.jpg"},
       {titulo:"El Exorcista", director:"William Friedkin", imagen:"https://www.ecartelera.com/carteles/5100/5147/001_m.jpg"}
     ]
   }
